2f28b1162d821abe1a354cea14b8812e1c9a64bc
Erivan Cledson <erivancled@hotmail.com>
2022-06-16 15:00:50 -0300

    Nova versão

---
+++ src/app/service/alticci.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlticciService {
+
+  constructor(private http: HttpClient) { }
+
+  public calcular(valor: number): Observable<number>{
+    return this.http.get<number>('http://localhost:8080/alticci/'+valor);
+  }
+
+}
